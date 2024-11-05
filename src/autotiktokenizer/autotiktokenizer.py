d32dc77a20d2807ba0e9dba006a9b7c5493dc3a2
bhavnicksm <bhavnicksm@gmail.com>
2024-11-06 00:55:48 +0530

    Bump version to 0.1.1 and enhance _AutoTikTokenizer functionality

--- src/autotiktokenizer/autotiktokenizer.py
+++ src/autotiktokenizer/autotiktokenizer.py
@@ -2,10 +2,11 @@ from tokenizers import Tokenizer
 import json
 import tiktoken
 
+
 class _AutoTikTokenizer:
     """
-    _AutoTikTokenizer is a class designed to interface with HuggingFace tokenizers to provide a TikToken tokenizer 
-    that can be used for the tokenization process. It mimics the functionality of AutoTokenizer in HuggingFace 
+    _AutoTikTokenizer is a class designed to interface with HuggingFace tokenizers to provide a TikToken tokenizer
+    that can be used for the tokenization process. It mimics the functionality of AutoTokenizer in HuggingFace
     but is tailored for TikToken.
     Attributes:
         tokenizer (Tokenizer): The HuggingFace tokenizer instance.
@@ -31,66 +32,87 @@ class _AutoTikTokenizer:
         __call__():
             Returns the TikToken encoding.
     """
-    def __init__(self):
-        self.tokenizer = None
-        self.name = None
-        self.vocab = None
-        self.tokenizer_config = None
-        self.mergeable_ranks = None
-        self.special_tokens = None
-        self.pattern = None
-
-    def get_mergable_ranks(self):
-        # Convert vocab to binary mergeable_ranks
-        self.mergeable_ranks = {}
-        
-        # Sort vocab by token id to ensure correct ordering
-        sorted_vocab = sorted(self.vocab.items(), key=lambda x: x[1])
-        
-        # Create binary format ranks starting from 1
-        for rank, (token, _) in enumerate(sorted_vocab, start=0):
-            # Handle GPT-2 style tokens
-            if token.startswith('Ġ'):
-                token = ' ' + token[1:]
-            self.mergeable_ranks[token.encode('utf-8')] = rank
-        
-        return self.mergeable_ranks
+    def __init__(self) -> None:
+        self.bytes_encoder = self._bytes_to_unicode()
+        self.bytes_decoder = {v:k for k,v in self.bytes_encoder.items()}
     
+    def _bytes_to_unicode(self):
+        """
+        Returns list of utf-8 byte and a corresponding list of unicode strings.
+        The reversible bpe codes work on unicode strings.
+        This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+        When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+        This is a signficant percentage of your normal, say, 32K bpe vocab.
+        To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+        And avoids mapping to whitespace/control characters the bpe code barfs on.
+        """
+        bs = list(range(ord("!"), ord("~")+1))+list(range(ord("¡"), ord("¬")+1))+list(range(ord("®"), ord("ÿ")+1))
+        cs = bs[:]
+        n = 0
+        for b in range(2**8):
+            if b not in bs:
+                bs.append(b)
+                cs.append(2**8+n)
+                n += 1
+        cs = [chr(n) for n in cs]
+        return dict(zip(bs, cs))
+    
+    def _normalize_token_bytes(self, token):
+        """Convert bytes to unicode."""
+        result = bytearray([self.bytes_decoder[b] for b in token])
+        result = bytes(result)
+        return result
+
+    def get_mergable_ranks(self, vocab, special_tokens):
+        """Convert vocab to binary mergeable_ranks."""
+        self.mergeable_ranks = {}
+        sorted_vocab = sorted(vocab.items(), key=lambda x: x[1])
+        for rank, (token, _) in enumerate(sorted_vocab, start=0):
+            if token not in special_tokens:
+                key = self._normalize_token_bytes(token)
+            else:
+                key = token.encode()
+            self.mergeable_ranks[key] = rank
+        return self.mergeable_ranks
+
     def get_special_tokens(self):
         self.special_tokens = {}
         sp = self.tokenizer.get_added_tokens_decoder()
         for idx, token in sp.items():
             self.special_tokens[token.content] = idx
         return self.special_tokens
-    
+
     def get_pattern_str(self):
         self.pattern = r'(?:[sdmt]|ll|ve|re)| ?\p{L}++| ?\p{N}++| ?[^\s\p{L}\p{N}]++|\s++$|\s+(?!\S)|\s'
         return self.pattern
 
-    def get_tiktoken_encoding(self):
+    def get_tiktoken_encoding(self, vocab):
         special_tokens = self.get_special_tokens()
-        mergeable_ranks = self.get_mergable_ranks()
+        mergeable_ranks = self.get_mergable_ranks(vocab, special_tokens)
         pattern = self.get_pattern_str()
 
         encoding = tiktoken.Encoding(
-            self.name, 
+            self.name,
             pat_str=pattern,
             mergeable_ranks=mergeable_ranks,
             special_tokens=special_tokens,
         )
-        
+
         return encoding
-    
+
     def from_pretrained(self, tokenizer_name_or_path: str):
         self.tokenizer_name_or_path = tokenizer_name_or_path
         self.tokenizer = Tokenizer.from_pretrained(tokenizer_name_or_path)
-        self.vocab = self.tokenizer.get_vocab()
+        vocab = self.tokenizer.get_vocab()
+
         self.tokenizer_config = dict(json.loads(self.tokenizer.to_str()))
         self.name = self.tokenizer_name_or_path.split('/')[-1]
-        return self.get_tiktoken_encoding()
-        
-    def __call__(self):
-        return self.get_tiktoken_encoding()
+        return self.get_tiktoken_encoding(vocab)
 
+    def __call__(self, tokenizer_name_or_path: str):
+      return self.from_pretrained(tokenizer_name_or_path)
+    
+    def __repr__(self) -> str:
+        return "AutoTikTokenizer"
 
 AutoTikTokenizer = _AutoTikTokenizer()
